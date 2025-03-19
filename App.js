d963f875220ef3e7e77fd14ade7135e7549bae6e
Emanuellacgomes <168464880+Emanuellacgomes@users.noreply.github.com>
2025-03-19 17:18:09 -0300

    Importação de Novas bibiliotecas
    blá blá blá

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import {StyleSheet, Text, View} from 'react-native';
 import Login from './screens/login';
 import Home from './screens/home';
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
 export default function App() {
   return (
     <View style={styles.container}>
